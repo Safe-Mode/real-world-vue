50da91463238fcf4546e7fdb586854529104d11f
Safe_Mode <safe2mode@gmail.com>
2019-02-24 13:32:29 +0300

    Make API call to get data through axios

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,16 +2,32 @@
   <div>
     <h1>Event Listing</h1>
 
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import EventCard from '@/components/EventCard.vue'
 
 export default {
+  data() {
+    return {
+      events: []
+    }
+  },
   components: {
     EventCard
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events')
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(err => {
+        console.log(err.response)
+      })
   }
 }
 </script>
