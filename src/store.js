a4f709b5a8ba928e3df6e7ed4035d90c637a7ed0
Safe_Mode <safe2mode@gmail.com>
2019-11-03 15:09:22 +0300

    Optimize vuex state

--- src/store.js
+++ src/store.js
@@ -61,14 +61,20 @@ export default new Vuex.Store({
           console.log(err.response)
         })
     },
-    fetchEvent({ commit }, id) {
-      EventService.getEvent(id)
-        .then(response => {
-          commit('SET_EVENT', response.data)
-        })
-        .catch(error => {
-          console.log('There was an error:', error.response)
-        })
+    fetchEvent({ commit, getters }, id) {
+      const event = getters.getEventById(id)
+
+      if (event) {
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(id)
+          .then(response => {
+            commit('SET_EVENT', response.data)
+          })
+          .catch(error => {
+            console.log('There was an error:', error.response)
+          })
+      }
     }
   }
 })
