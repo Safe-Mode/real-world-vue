b5dad58bd0f88f58d3bc6158c1157b781b9b764e
Safe_Mode <safe2mode@gmail.com>
2019-11-05 14:14:32 +0300

    Implement base input

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input :value="value" @input="updateValue" v-bind="$attrs" />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    value: [Number, String],
+    label: String
+  },
+  methods: {
+    updateValue(evt) {
+      this.$emit('input', evt.target.value)
+    }
+  }
+}
+</script>
